1c6d6be26434d232dc5713015759c19196da88b7
min731 <ypd04208@gmail.com>
2023-10-17 13:25:08 +0900

    Serving_face_registration_10_17

---
+++ jm/Family_Album/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    from .views import main_views,face_registration_views
+    app.register_blueprint(main_views.bp)
+    app.register_blueprint(face_registration_views.bp)
+
+    return app
+
+# set FLASK_APP=Family_Album
+# set FLASK_DEBUG=True
+# set FLASK_RUN_PORT=5000

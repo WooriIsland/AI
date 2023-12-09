5d23bdb05cc76715c789a218e9509fcd1dc0e41a
blessian <blessian.dev@gmail.com>
2023-12-10 00:15:51 +0900

    fix: punching on the mad cat

--- blessian/lang_agency/tools.py
+++ blessian/lang_agency/tools.py
@@ -30,7 +30,7 @@ The input for this tool should be the JSON including 'schedule_management_type',
         name="general_conversation",
         description="""
 Using this tool is mandatory when 'next_action' is 'general_conversation'.
-이 도구의 입력은 챗봇이 사용자에게 안내해야 하는 진행상황을 요약한 한국어 문장입니다.
+The input for this tool is a summarized Korean sentence indicating the progress that the chatbot needs to guide the user through.
 """,
         func=chains.conversation_chain.run,
         return_direct=True,
@@ -45,7 +45,6 @@ The input for this tool is a JSON with the keys 'period,' indicating the time to
 The 'period' must be one of '01월', '02월', '03월', '04월', '05월', '06월', '07월', '08월', '09월', '10월', '11월', '12월,' and if the month cannot be specified, it is '개최중'.
 The 'region' must be one of '서울', '인천', '대전', '대구', '광주', '부산', '울산', '세종시', '경기도', '강원도', '충청북도', '충청남도', '경상북도', '경상남도', '전라북도', '전라남도', '제주도,' and if the region cannot be estimated, it is '지역'.
 The result of this tool is a JSON with 'recommend' and 'next_action' fields. 'recommend' contains information about events or festivals, and the 'next_action' is always 'general_conversation.'
-이 도구의 출력은 항상 한국어입니다.
 """,
         func=get_event_info.get_events,
         return_direct=False,
